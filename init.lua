d0ed055763028b74d7b25b1bdf1ddd8ca50e0267
Aleks <am@klaut.org>
2017-08-22 16:59:50 +0200

    yolo

--- init.lua
+++ init.lua
@@ -19,7 +19,7 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Down", function()
   local screen = win:screen()
   local max = screen:frame()
 
-  f.x = max.x + 350
+  f.x = max.x + (max.w / 6)
   f.y = max.y
   f.w = max.w - (max.w / 6 * 2)
   f.h = max.h
@@ -78,32 +78,4 @@ hs.hotkey.bind({"ctrl", "cmd"}, "J", arrow_down,  nil, arrow_down)
 hs.hotkey.bind({"ctrl", "cmd"}, "K", arrow_up,    nil, arrow_up)
 hs.hotkey.bind({"ctrl", "cmd"}, "L", arrow_right, nil, arrow_right)
 
-
--- Second Display, Split between Apps
--- hs.hotkey.bind({"cmd", "alt", "ctrl"}, "H", function()
---   local leftScreen = "<display name>"
---   local windowLayout = {
---       {"Safari", nil, leftScreen, {x=0, y=0, w=0.6, h=1}, nil, nil},
---       {"Terminal", nil, leftScreen, {x=0, y=0, w=0.6, h=1}, nil, nil},
---       {"Skype", nil, leftScreen, {x=0.6, y=0, w=0.4, h=1}, nil, nil},
---   }
---   hs.layout.apply(windowLayout)
--- end)
-
--- Second Display, Split between Apps, Setting 2
--- hs.hotkey.bind({"cmd", "alt", "ctrl"}, "L", function()
---   local leftScreen = "<display name>"
---   local windowLayout = {
---       {"Safari", nil, leftScreen, {x=0.4, y=0, w=0.6, h=1}, nil, nil},
---       {"Terminal", nil, leftScreen, {x=0.4, y=0, w=0.6, h=1}, nil, nil},
---       {"Skype", nil, leftScreen, {x=0, y=0, w=0.4, h=1}, nil, nil},
---   }
---   hs.layout.apply(windowLayout)
--- end)
-
-
--- Reload config via mapping
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
-  hs.reload()
-end)
 hs.alert.show("Config loaded")
